6d39ea340f2a58ee4e0b9bf9593c0aa38151a8e4
cx3hen <1206726916@qq.com>
2023-04-22 23:56:16 +0800

    feat: 添加了Dropout层、更长的时间窗口（10天），并调整了部分超参数。

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ import numpy as np
 import tensorflow as tf
 import os
 
+
 def build_multivariate_lstm_model():
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.LSTM(128, input_shape=(None, 4), return_sequences=True))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.LSTM(64, return_sequences=True))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.LSTM(32, return_sequences=False))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(1))
     return model
 
-stock_codes = ['000001', '000002', '000004', '000005', '000006', '000007', '000008', '000009', '000010', '000011', '000012', '000014', '000016', '000017', '600004']
+
+def create_dataset(data, time_steps=1):
+    X, Y = [], []
+    for i in range(len(data) - time_steps):
+        X.append(data.iloc[i:(i + time_steps)].values)
+        Y.append(data.iloc[i + time_steps]['Close'])
+    return np.array(X), np.array(Y)
+
+
+stock_codes = ['000001', '000002', '000004', '000005', '000006', '000007', '000008', '000009', '000010', '000011',
+               '000012', '000014', '000016', '000017', '600004']
+
+time_steps = 10
 
 for code in stock_codes:
     df = pd.read_csv(code + '.csv', encoding='GBK')
@@ -26,22 +42,26 @@ for code in stock_codes:
     train_data = features[:train_size]
     test_data = features[train_size:]
 
+    X_train, Y_train = create_dataset(train_data, time_steps)
+    X_test, Y_test = create_dataset(test_data, time_steps)
+
     model = build_multivariate_lstm_model()
     model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=['mae'])
-    model.fit(train_data.values.reshape(-1, 1, 4), train_data['Close'].values.reshape(-1, 1), epochs=100, batch_size=32, verbose=2)
+    model.fit(X_train, Y_train, epochs=100, batch_size=32, verbose=2)
 
-    test_loss, test_mae = model.evaluate(test_data.values.reshape(-1, 1, 4), test_data['Close'].values.reshape(-1, 1), verbose=0)
-    print('股票代码 {} 的测试 MAE: {:.4f}, 测试损失: {:.4f}'.format(code, test_mae, test_loss))
+    test_loss, test_mae = model.evaluate(X_test, Y_test, verbose=0)
+    print('股票代码 {} 的测试损失 loss: {:.4f}, 测试 MAE: {:.4f}'.format(code, test_loss, test_mae))
 
     future_data = []
-    last_data = train_data[-1:][['Open', 'High', 'Low', 'Close']].values.reshape(-1, 1, 4)
+    last_data = train_data[-time_steps:][['Open', 'High', 'Low', 'Close']].values.reshape(-1, time_steps, 4)
     for i in range(30):
         predict = model.predict(last_data)
         future_data.append(predict[0][0])
 
-        new_data = np.zeros((1, 1, 4))
-        new_data[:, :, :3] = last_data[:, :, 1:4]
-        new_data[:, :, 3] = predict
+        new_data = np.zeros((1, time_steps, 4))
+        new_data[:, :-1, :] = last_data[:, 1:, :]
+        new_data[:, -1, :3] = last_data[:, -1, 1:4]
+        new_data[:, -1, 3] = predict
         last_data = new_data
 
     future_data = np.array(future_data) * stds['Close'] + means['Close']
